a528c236b409b630b22be86956de2ebf6d01155f
Thiago Romão <thiaroll@gmail.com>
2019-11-08 22:08:23 -0300

    isso ai mesmo via dar certo

--- script.js
+++ script.js
@@ -1,83 +1,30 @@
-/**
- * At least two points are needed to interpolate something.
- * @class Lagrange polynomial interpolation.
- * The computed interpolation polynomial will be reffered to as L(x).
- * @example
- * var l = new Lagrange(0, 0, 1, 1);
- * var index = l.addPoint(0.5, 0.8);
- * console.log(l.valueOf(0.1));
- * 
- * l.changePoint(index, 0.5, 0.1);
- * console.log(l.valueOf(0.1));
- */
-var Lagrange = function(x1, y1, x2, y2) {
+function resolverDois(){
+	let x0 = Number(document.getElementById("x0").value);
+	let x1 = Number(document.getElementById("x1").value);
+	let fx0 = Number(document.getElementById("fx0").value);
+	let fx1 = Number(document.getElementById("fx1").value);
+	let ponto = Number(document.getElementById("ponto").value);
 	
-	this.xs = [x1, x2];
-	this.ys = [y1, y2];
-	this.ws = [];
-	this._updateWeights();
-}
-
-/**
- * Adds a new point to the polynomial. L(x) = y
- * @return {Number} The index of the added point. Used for changing the point. See changePoint.
- */
-Lagrange.prototype.addPoint = function(x, y) {
-	this.xs.push(x);
-	this.ys.push(y);
-	this._updateWeights();
-	return this.xs.length-1;
-}
-
-/**
- * Changes a previously added point.
- */
-Lagrange.prototype.changePoint = function(index, x, y) {
-	this.xs[index] = x;
-	this.ys[index] = y;
-	this._updateWeights();
-}
-
-/**
- * Recalculate barycentric weights.
- */
-Lagrange.prototype._updateWeights = function() {
-	var k = this.xs.length;
-	var w;
+	let a = ((ponto - x1) / (x0 - x1)); 
+	let b = ((ponto - x0) / (x1 - x0));
 	
-	for (var j = 0; j < k; ++j) {
-		w = 1;
-		for (var i = 0; i < k; ++i) {
-			if (i != j) {
-				w *= this.xs[j] - this.xs[i];
-			}
-		}
-		this.ws[j] = 1/w;
-	}
+	let c = (a*fx0) + (b*fx1);
+
+	return c;
 }
 
-/**
- * Calculate L(x)
- */
-Lagrange.prototype.valueOf = function(x) {
-	var a = 0;
-	var b = 0;
-	var c = 0;
-	
-	for (var j = 0; j < this.xs.length; ++j) {
-		if (x != this.xs[j]) {
-			a = this.ws[j] / (x - this.xs[j]);
-			b += a * this.ys[j];
-			c += a;
-		} else {
-			return this.ys[j];
-		}
-	}
-	
-	return b / c;
-}
+function resolveTres(){
+	let x0 = Number(document.getElementById("x0").value);
+	let x1 = Number(document.getElementById("x1").value);
+	let x2 = Number(document.getElementById("x2").value);
+	let fx0 = Number(document.getElementById("fx0").value);
+	let fx1 = Number(document.getElementById("fx1").value);
+	let fx2 = Number(document.getElementById("fx1").value);
+	let ponto = Number(document.getElementById("ponto").value);
 
-function pau_preto ()
-{
-	console.log("PAU PRETO");
+	let a = ((ponto - x1) / (x0 - x1)) * ((ponto - x2) / (x0 - x2)); 
+	let b =  ((ponto - x0) / (x1 - x0)) * ((ponto - x2 / (x1 - x2)));
+	let c = ((ponto - x0) / (x2 - x0)) * ((ponto - x1 / (x2 - x1)));
+
+	let d = (a*fx0) + (b*fx1) + (c*fx2);
 }
